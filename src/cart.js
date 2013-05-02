59e884aaccd1402d82a31e38d88115747870d19a
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2013-05-02 15:06:36 +0200

    Made it clean

--- src/cart.js
+++ src/cart.js
@@ -2,22 +2,4 @@ var MyCart = MyCart || {}
 
 MyCart.Cart = function(){
 	var self = this;
-
-	self.items = [];
-
-	self.addItem = function(item){
-		self.items.push(item);
-		self.itemWasAdded(item.sku);
-	};
-
-	self.itemCount = function(){
-		return self.items.length;
-	}
-
-	self.itemWasAdded = function(){
-	}
-
-	self.removeItem = function(item)	{
-		self.items.remove(item);
-	};
 }
